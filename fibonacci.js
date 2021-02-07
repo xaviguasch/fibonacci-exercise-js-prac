bd1226d179ba41a9f19f1d3c67b0fddb49a37cb4
xaviguasch <xaviguasch@gmail.com>
2021-02-07 17:28:35 +0100

    added fibonacci solution

--- fibonacci.js
+++ fibonacci.js
@@ -3,6 +3,19 @@ function fibonacciGenerator(n) {
   //Write your code here:
   //Return an array of fibonacci numbers starting from 0.
   //Do NOT change any of the code below 👇
+
+  const arr = []
+  for (let i = 1; i <= n; i++) {
+    if (i === 1) {
+      arr.push(0)
+    } else if (i === 2) {
+      arr.push(1)
+    } else {
+      arr.push(arr[i - 3] + arr[i - 2])
+    }
+  }
+
+  return arr
 }
 
-console.log(fibonacciGenerator(15))
+console.log(fibonacciGenerator(5))
